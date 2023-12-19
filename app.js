7346cc14b44049d298c802ab846c190303c2a1b3
Arthur <arthur4456645@gmail.com>
2023-12-19 17:12:08 +0800

    fixed bug in app.js(id relate to dtail.hbs))

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 const restaurants = require('./public/jsons/restaurant.json').results
@@ -14,13 +14,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.render('index.hbs', { restaurants })
+  res.render('index.hbs', {restaurants})
 })
 
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
-  const restaurant = restaurants.find((results) => results.id.toString() === id)
-  res.render('detail.hbs', { restaurant })
+  const restaurantInfo = restaurants.find((results) => results.id.toString() === id)
+  res.render('detail.hbs', {restaurantInfo})
 })
 
 app.listen(port, () => {
